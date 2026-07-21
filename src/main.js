8678d378d7b6c5e39d21dea59da0a1ea42660a87
ThanhLong <longb2306628@student.ctu.edu.vn>
2026-07-21 20:22:46 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import './assets/main.css'
-
+import './assets/form.css';
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const app = createApp(App)
 
